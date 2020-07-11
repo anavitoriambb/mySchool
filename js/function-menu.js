9c5e5f624f7931ec60c5c7cc03c794c92c2ec828
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2020-07-11 17:37:52 -0300

    commit

---
+++ js/function-menu.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    //Se for id = #; se for class = .
+    $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+})
--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -12,7 +12,7 @@ $username = "root";
 $password = "";
 
 if($conecta = mysqli_connect($hostname, $username, $password, $database)){
-    echo 'Conectado ao banco de dados'.$database.'...';
+    //echo 'Conectado ao banco de dados '.$database.'...';
 }else{
     echo 'Erro: '.mysqli_connect_error();
 }
