2eb21e8b05b670774093711b6f3236d7d0fb0637
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2020-07-13 08:14:43 -0300

    commit

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     //Se for id = #; se for class = .
-    $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+
+    //$('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+    //Criando uma funçãpn para adicionar o formulário de cadastro na tela pelo botão #add-disciplinas
+    $('#add-disciplinas').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('conteudo').load('src/disciplinas/visao/add-disciplinas')
+    })
+
 })
