1b4a326cd27f5864a19f738e040ced2f4d9ea974
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2020-09-21 22:58:11 -0300

    alunos

--- js/function-menu.js
+++ js/function-menu.js
@@ -15,4 +15,18 @@ $(document).ready(function() {
         $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
     })
 
+    //$('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+    //Criando uma funçãpn para adicionar o formulário de cadastro na tela pelo botão #add-disciplina
+    $('#add-alunos').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/adiciona-alunos.html')
+    })
+
+    $('#list-alunos').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/list-alunos.html')
+    })
+
 })
